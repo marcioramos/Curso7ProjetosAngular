0908f5c1e5237b890f3c1c660ac5878b7cc5e2d9
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-02 16:32:17 -0300

    Aula 30

--- calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -12,4 +12,41 @@ describe('CalculadoraService', () => {
   it('should ...', inject([CalculadoraService], (service: CalculadoraService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let resultado = service.calucar(1, 4, CalculadoraService.SOMA);
+      expect(resultado).toEqual(5);
+    })
+  );
+
+  it('deve garantir que 1 - 4 = -3',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let resultado = service.calucar(1, 4, CalculadoraService.SUBTRACAO);
+      expect(resultado).toEqual(-3);
+    })
+  );
+
+  it('deve garantir que 1 / 4 = 0.25',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let resultado = service.calucar(1, 4, CalculadoraService.DIVISAO);
+      expect(resultado).toEqual(0.25);
+    })
+  );
+
+  it('deve garantir que 1 * 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let resultado = service.calucar(1, 4, CalculadoraService.MULTIPLICACAO);
+      expect(resultado).toEqual(4);
+    })
+  );
+
+  it('deve returnar 0 para operação inválida',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let resultado = service.calucar(1, 4, '%');
+      expect(resultado).toEqual(0);
+    })
+  );
+
+
 });
