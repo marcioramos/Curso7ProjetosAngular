16f4be522c10bdba63456001288fe4be5f0e0616
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-02 08:02:39 -0300

    Aula 24

--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './componentes';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    CalculadoraComponent
+  ],
+  exports:[
+    CalculadoraComponent
+  ]
 })
 export class CalculadoraModule { }
