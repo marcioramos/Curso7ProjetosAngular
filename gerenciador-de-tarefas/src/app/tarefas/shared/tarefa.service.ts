b53ed9ea0d5cf02a15b0ac08948a218618732987
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-08 16:59:53 -0300

    Aula 41

---
+++ gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Tarefa } from './';
+
+@Injectable()
+export class TarefaService {
+
+  constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+}
--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TarefaService } from './shared/tarefa.service';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [ TarefaService ]
 })
 export class TarefasModule { }
