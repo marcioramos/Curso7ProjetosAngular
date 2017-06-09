8f7e8b79c2cdabcfe6016b44c6240eee02d96362
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-09 08:56:36 -0300

    Aula 47

--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { TarefaService } from './shared/tarefa.service';
 import { ListarTarefaComponent } from './listar';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
-  declarations: [ListarTarefaComponent],
-  providers: [TarefaService]
+  declarations: [
+    ListarTarefaComponent
+  ],
+  providers: [
+    TarefaService
+  ]
 })
 export class TarefasModule { }
