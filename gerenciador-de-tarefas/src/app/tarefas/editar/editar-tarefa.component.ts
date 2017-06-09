5b16884f9912fd8f1f2a48bef5030685d0c74637
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-09 17:04:40 -0300

    Aula 56

--- gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
+++ gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
-import { TarefaService, Tarefa } from '../shared/';
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-editar-tarefa',
@@ -9,9 +11,25 @@ import { TarefaService, Tarefa } from '../shared/';
 })
 export class EditarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa') formTarefa: NgForm;
+  tarefa: Tarefa;
+
+  constructor(
+    private tarefaService: TarefaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.tarefa = this.tarefaService.buscarPorId(id);
+  }
+
+  atualizar(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.atualizar(this.tarefa);
+      this.router.navigate(["/tarefas"]);
+    }
   }
 
 }
