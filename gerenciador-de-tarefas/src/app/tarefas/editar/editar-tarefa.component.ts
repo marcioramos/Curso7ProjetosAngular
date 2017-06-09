427c1099a92eec97f425c7b3a71d9b5bd3cd2797
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-09 16:40:48 -0300

    Aula 54

---
+++ gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { TarefaService, Tarefa } from '../shared/';
+
+@Component({
+  selector: 'app-editar-tarefa',
+  templateUrl: './editar-tarefa.component.html',
+  styleUrls: ['./editar-tarefa.component.css']
+})
+export class EditarTarefaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
