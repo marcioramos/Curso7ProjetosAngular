86982f50f4cae80675883f152d0302781256f946
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-16 22:21:06 -0300

    Aula 106

--- dashboard/src/app/app.component.spec.ts
+++ dashboard/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        DashboardModule
+      ]
     }).compileComponents();
   }));
 
