f001187713b2413abcf123153ad4c89b31ab63f9
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-06-17 17:43:44 -0300

    Aula 124

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './dashboard';
 import { CalculadoraModule } from './calculadora';
+import { ConversorModule } from './conversor';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { CalculadoraModule } from './calculadora';
     HttpModule,
     DashboardModule,
     CalculadoraModule,
+    ConversorModule,
     AppRoutingModule
   ],
   providers: [],
